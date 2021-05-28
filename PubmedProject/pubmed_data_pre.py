3c9b9dbc728aa2061ea7fc078196e01dfeff45ae
pinkishincoloragain <smb1103@gmail.com>
2021-05-29 01:55:56 +0900

    changed pubmed_data_pre.py and added no_nextline version. Starting data format transformation

--- PubmedProject/pubmed_data_pre.py
+++ PubmedProject/pubmed_data_pre.py
@@ -1,5 +1,11 @@
 import os
+import re
 
+start = re.compile(r"^\d*[.]")
+year = re.compile(r"20\d\d")
+n = re.compile(r"\n")
+
+year_pattern = re.compile
 if __name__ == '__main__':
     path = 'data/'
     file_list = os.listdir(path)
@@ -7,20 +13,25 @@ if __name__ == '__main__':
         filename = file_list[i]
         print(f"current file : {filename}")
         with open("data/"+filename,"r",encoding="utf-8") as infile:
-            with open ('result/'+filename +' result.csv',"w+",encoding='utf-8') as outfile:
+            with open('result/'+filename +' result.txt',"w+",encoding='utf-8') as outfile:
                 temp = infile.read()
                 splitted = list(temp.split("\n\n"))
                 for item in splitted:
+                    item.strip()
+                    if 'doi' in item and year.search(item):
+                        item = re.sub(pattern=n,repl='',string=item)
+                        outfile.write(item)
+                        outfile.write('\n\n')
+                    # else: print("not!!!!!: ",item)
+
                     if len(item) > 200:
                         if "Author information:" in item:
                             continue
                         # outfile.write(item)
-                        item.replace("\n"," ")
-                        item.replace("\r\n", " ")
-                        item.replace("\r", " ")
+                        item = re.sub(pattern=n,repl='',string=item)
                         dotsplit = list(item.split("."))
-                        for sen in dotsplit:
-                            outfile.write(sen)
-                            # outfile.write('.')
-                            # outfile.write("\n")
-                        outfile.write('\n\n')
+                        # print(dotsplit)
+                        for i in range(len(dotsplit)):
+                            if len(dotsplit[i]) > 0:
+                                outfile.write(str(dotsplit[i].lstrip()) + ".\n")
+                        outfile.write('\n')
---
+++ PubmedProject/pubmed_data_pre_no_nextline_ver.py
@@ -0,0 +1,37 @@
+import os
+import re
+
+start = re.compile(r"^\d*[.]")
+year = re.compile(r"20\d\d")
+n = re.compile(r"\n")
+
+year_pattern = re.compile
+if __name__ == '__main__':
+    path = 'data/'
+    file_list = os.listdir(path)
+    for i in range(len(file_list)):
+        filename = file_list[i]
+        print(f"current file : {filename}")
+        with open("data/"+filename,"r",encoding="utf-8") as infile:
+            with open('result2/'+filename +' result.txt',"w+",encoding='utf-8') as outfile:
+                temp = infile.read()
+                splitted = list(temp.split("\n\n"))
+                for item in splitted:
+                    item.strip()
+                    if 'doi' in item and year.search(item):
+                        item = re.sub(pattern=n,repl='',string=item)
+                        outfile.write(item)
+                        outfile.write('\n\n')
+                    # else: print("not!!!!!: ",item)
+
+                    if len(item) > 200:
+                        if "Author information:" in item:
+                            continue
+                        # outfile.write(item)
+                        item = re.sub(pattern=n,repl='',string=item)
+                        dotsplit = list(item.split("."))
+                        # print(dotsplit)
+                        for i in range(len(dotsplit)):
+                            if len(dotsplit[i]) > 0:
+                                outfile.write(str(dotsplit[i].lstrip()) + ". ")
+                        outfile.write('\n\n')
