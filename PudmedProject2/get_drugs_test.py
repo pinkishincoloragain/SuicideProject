52edfa8585373df9ab9153ad07d09eef44c853fb
pinkishincoloragain <smb1103@gmail.com>
2021-06-28 19:15:10 +0900

    changing exceptions - for name rules

--- PudmedProject2/get_drugs_test.py
+++ PudmedProject2/get_drugs_test.py
@@ -19,39 +19,50 @@ def from_excel(**kwargs):
         for sublist in drugs.values.tolist():
             for item in sublist:
                 if type(item) is str:
-                    flat_list.append(item.replace("\u3000"," "))
+                    flat_list.append(item.replace("\u3000",""))
+        return list(set(flat_list))
     else:
         print(f"Getting druglist from {path}, using columns: {' '.join(columns)}")
         for sublist in drugs.loc[:,columns].values.tolist():
             for item in sublist:
                 if type(item) is str:
                     flat_list.append(item.replace("\u3000"," "))
+        drop_exp = []
 
-    drop_exp = []
-    for item in flat_list:
-        if "," in item:
-            idx = item.find(",")
-            item = item[:idx]
-            drop_exp.append(item.strip())
 
-        if "(" in item:
-            item = re.sub(pattern=pattern1,repl="",string=item)
-            drop_exp.append(item.strip())
+        for item in flat_list:
+            origin = item
+            if "," in item:
+                print("a ", item)
+                idx = item.find(",")
+                item = item[:idx]
+                print("b ", item)
+                drop_exp.append(item.strip())
 
-        else:
-            drop_exp.append(item.strip())
+            if "(" in item:
+                print("i-before ", item)
+                item = re.sub(pattern=pattern1,repl="",string=item)
+                print("i-after ", item)
+                drop_exp.append(item.strip())
 
-    res = list(set(drop_exp))
-    for item in res:
-        for excp in excp_dict:
-            if excp in item:
+            else:
+                drop_exp.append(item)
+
+        res = list(set(drop_exp))
+        for item in res:
+            for excp in excp_dict:
+                if excp in item:
+                    print("rmv: ",excp, item)
+                    res.remove(item)
+                    break
+            if item in replace_dict.keys():
+                print("rpl: ", item,":", replace_dict[item])
+                # item = replace_dict[item]
                 res.remove(item)
-                break
-        if item in replace_dict.keys():
-            res.remove(item)
-            res.append(replace_dict[item])
+                res.append(replace_dict[item])
 
     return list(set(res))
+
     # return res.values.tolist()
 
 def from_csv(**kwargs):
--- PudmedProject2/tester.py
+++ PudmedProject2/tester.py
@@ -1,6 +1,6 @@
 import get_drugs
 # drugarray=get_drugs.from_excel(path="Drug_mapping_v2.xlsx", column=["ingredient", "ingredient_1"])
-# drugarray=get_drugs.from_excel(path="Drug_mapping_v2.xlsx", column=["ingredient", "ingredient_1"])
-drugarray=get_drugs.from_excel(path="Drug_mapping_v2.xlsx")
+drugarray=get_drugs.from_excel(path="Drug_mapping_v2.xlsx", columns=["ingredient", "ingredient_1"])
+# drugarray=get_drugs.from_excel(path="Drug_mapping_v2.xlsx")
 
 print(drugarray)
