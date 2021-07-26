db5a9de65a3206b8737fb9f189819bff1b23787c
pinkishincoloragain <smb1103@gmail.com>
2021-07-26 22:33:46 +0900

    added version 2s again

--- PubmedProject3/get_abstracts_MAIN_ver2.py
+++ PubmedProject3/get_abstracts_MAIN_ver2.py
@@ -17,7 +17,6 @@ abstracts, drugs, queries = get_abstracts_pubmed.main(email="smb1103@gmail.com",
                                                       max_results=100000, from_date="1990/01/01", to_date="2021/07/26", mesh=False, case_report=True)
 output_file = "data/abstracts_only_suicidal.xlsx"
 
-
 # Using only ingredients, no meSH
 # columns = ['ingredient_1', 'ingredient_2', 'ingredient_3', 'ingredient_4', 'ingredient_5', 'ingredient_6', 'ingredient_7', 'ingredient_8', 'ingredient_9']
 # abstracts, drugs, queries = get_abstracts_pubmed.main(email="smb1103@gmail.com", druglist_path="drug_mapping_v3_210726_2.xlsx", columns=columns,
