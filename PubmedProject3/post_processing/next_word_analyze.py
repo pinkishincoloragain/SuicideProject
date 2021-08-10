118f3c99cf2177431d1f853fc7629f2baf0d94db
pinkishincoloragain <smb1103@gmail.com>
2021-08-10 13:55:52 +0900

    added new temp file

--- PubmedProject3/post_processing/next_word_analyze.py
+++ PubmedProject3/post_processing/next_word_analyze.py
@@ -2,7 +2,7 @@
 # 이건 간단해서 읽을 수 있지 않을까..!
 # 두 번째 오는 단어들을 set으로 반환함.
 if __name__ == "__main__":
-    with open("temp.txt","r") as infile:
+    with open("temp2.txt","r", encoding='UTF8') as infile:
         with open("next_words_each.txt", "r") as outfile:
             data = infile.read()
             temp = data.split("\n")
